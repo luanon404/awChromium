dc7cf8123aef7e837f861ad148ec40df96703979
luanon404 <luanon404@gmail.com>
2023-10-15 01:42:48 +0700

    Fix some bugs

--- README.md
+++ README.md
@@ -2,14 +2,6 @@
 
 Android WebView separated from Chromium source code.
 
-# How to run?
+# Goal
 
-- I fixed all errors so you dont need do anything, enjoy!
-
-  | Step                                                                        | Demo                                                                                                   |
-  | --------------------------------------------------------------------------- | ------------------------------------------------------------------------------------------------------ |
-  | <pre><code>`git clone https://github.com/luanon404/AWChromium`</pre></code> | ![image](https://github.com/luanon404/AWChromium/assets/71830807/4a73bc0b-0915-4686-80d6-cbb12d1fbfea) |
-  | Click **_Settings_** > **_Project Structure_**                              | ![image](https://github.com/luanon404/AWChromium/assets/71830807/d73cb5ab-9367-45fe-95a7-5d79414ef64f) |
-  | Click **_SDK Location_** > **_OK_**                                         | ![image](https://github.com/luanon404/AWChromium/assets/71830807/367adb54-0135-48fb-ac33-26ac0ac05272) |
-
-- Then run like normal app.
+- Convert all Java code to Kotlin for easier processing.
--- aw_chromium/src/main/java/org/chromium/base/compat/ApiHelperForOMR1.kt
+++ aw_chromium/src/main/java/org/chromium/base/compat/ApiHelperForOMR1.kt
@@ -1,14 +1,12 @@
 // Copyright 2019 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+package org.chromium.base.compat
 
-package org.chromium.base.compat;
-
-import android.annotation.TargetApi;
-import android.os.Build;
-import android.view.Window;
-
-import org.chromium.base.annotations.VerifiesOnOMR1;
+import android.annotation.TargetApi
+import android.os.Build
+import android.view.Window
+import org.chromium.base.annotations.VerifiesOnOMR1
 
 /**
  * Utility class to use new APIs that were added in O_MR1 (API level 27). These need to exist in a
@@ -17,11 +15,10 @@ import org.chromium.base.annotations.VerifiesOnOMR1;
  */
 @VerifiesOnOMR1
 @TargetApi(Build.VERSION_CODES.O_MR1)
-public final class ApiHelperForOMR1 {
-    private ApiHelperForOMR1() {}
-
-    /** See {@link Window#isWideColorGamut() }. */
-    public static boolean isWideColorGamut(Window window) {
-        return window.isWideColorGamut();
+object ApiHelperForOMR1 {
+    /** See [Window.isWideColorGamut].  */
+    @JvmStatic
+    fun isWideColorGamut(window: Window): Boolean {
+        return window.isWideColorGamut
     }
 }
